67995492b752bc7c7c8ed6a5a666e6488f8ae6b0
Finn <anthony.torres0926@gmail.com>
2019-11-06 00:20:47 -0700

    Added start game, need to do button fixes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,57 @@
 // NO var EVER!!!
 
+//Randomizer
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
 //Caches
 const buttons = document.getElementById('buttons');
 const item1 = document.getElementById('item1');
 const item2 = document.getElementById('item2');
+const press = document.getElementById('randomizer');
+const attack = document.getElementById('atk');
+const defense = document.getElementById('def');
+const pGold = document.getElementById('pGold');
+const sGold = document.getElementById('sGold');
+const start = document.getElementById('start');
 
 let removed = [];
 
 //Player1
 let playerGold = 100;
 let playerInventory = [];
-let shopInventory = [];
+let playerStats = 0;
 
 //Shopkeeper
 let shopGold = 500;
+let shopInventory = [];
 
-function shopKeep() {
+//StartGame ? Render
+function startGame() {
+    
+    start.addEventListener('click', function() {
+        alert('Choose a class from below! or refresh the page to restart!')
+        removeButton();
+    });
+
+    chooseClass();
 };
+
+function chooseClass() {
+
+    press.addEventListener('click', function() {
+        playerStats1();
+        playerStats2();
+        alert('SIKE! You get random stats! Good luck!')
+        removeButton1();
+    });
+
+    playerG();
+    shopG();
+};
+
+startGame();
 
 //Items
 let items = [ // You can use indexs here?
@@ -38,9 +73,12 @@ item1.addEventListener('click', function(){
     if(playerGold >= items[0].cost) {
             playerInventory.push(items[0]);
             playerGold -= items[0].cost;
+            shopGold += items[0].cost;
             createElement1();
-    }
+    };
 
+    playerG();
+    shopG();
     removeElement1();
 });
 
@@ -49,9 +87,12 @@ item2.addEventListener('click', function(){
     if(playerGold >= items[1].cost) {
             playerInventory.push(items[1]);
             playerGold -= items[1].cost;
+            shopGold += items[1].cost;
             createElement2();
     }
 
+    playerG();
+    shopG();
     removeElement2();
 });
 
@@ -60,7 +101,7 @@ function createElement1() { // Add Parameters
     playerInv = document.createElement('ol');
     playerInv2 = document.getElementById('inv').getElementsByTagName('ul')[0];
     playerInv2.appendChild(playerInv);
-    playerInv.innerHTML = 'Sword';
+    playerInv.innerHTML = 'Sword'
 };
 
 function createElement2() {
@@ -68,6 +109,27 @@ function createElement2() {
     playerInv2 = document.getElementById('inv').getElementsByTagName('ul')[0];
     playerInv2.appendChild(playerInv);
     playerInv.innerHTML = 'Shield';
+};
+
+function playerStats1() {
+    playerStats = randomNum(3, 10);
+    attack.innerHTML = `${playerStats}`;
+};
+
+function playerStats2() {
+    playerStats = randomNum(3, 10);
+    defense.innerHTML = `${playerStats}`;
+};
+
+function playerG() {
+    playerGold;
+    pGold.innerHTML = `${playerGold}`;
+
+};
+
+function shopG() {
+    shopGold;
+    sGold.innerHTML = `${shopGold}`;
 };
 
 function removeElement1() {
@@ -79,3 +141,20 @@ function removeElement2() {
     shopInv = document.getElementById('item2');
     shopInv.disabled = true;
 };
+
+function removeButton() {
+    start.disabled = true;
+};
+
+function removeButton1() { //NOTE TO SELF :: DRY CODING! GAH! CHANGE IT ASAP!
+    disButtons = document.getElementById('dis1');
+    disButtons2 = document.getElementById('dis2');
+    disButtons3 = document.getElementById('dis3');
+    disButtons4 = document.getElementById('dis4');
+    disButtons.disabled = true;
+    disButtons2.disabled = true;
+    disButtons3.disabled = true;
+    disButtons4.disabled = true;
+};
+
+
